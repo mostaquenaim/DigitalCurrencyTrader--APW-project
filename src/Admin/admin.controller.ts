655a620121fb87cdeaec562b3fc5f734094fcf35
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-02-12 23:39:03 +0600

    new

--- src/Admin/admin.controller.ts
+++ src/Admin/admin.controller.ts
@@ -24,6 +24,7 @@ export class AdminController
 
   //add new body
   @Post('/add')
+  
   create(@Body() body) {
     return this.adminService.create(body);
   }
--- src/Admin/adminform.dto.ts
+++ src/Admin/adminform.dto.ts
@@ -1,13 +1,39 @@
-import { IsInt, IsNotEmpty, Length } from "class-validator";
+import { IsInt, IsNotEmpty, Length, Matches } from "class-validator";
 
 export class AdminForm {   
     @IsNotEmpty({message: "Please enter your id"}) 
-    @IsInt()
+    @IsInt({message: "id must be a number"})
+   // @Matches(/^[0-9]+$/,{message: "id must be a number"})
     id: number;
 
     @IsNotEmpty()
-    @Length(3,8)
+    @Length(4,25,{message: "name must be the size of between 4 and 25",})
     name: string;
+
+
+    @IsNotEmpty()
+    @Length(6,16,{message: "username must be the size of between 6 and 16"})
+    @Matches(/^[a-zA-Z0-9]+$/, {
+        message:'username must not have anything other than letters and numbers',
+      })
+    uname: string;
+
+    @IsNotEmpty()
+    @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{6,}$/, {
+        message:'Password must be equal or more than 6 characters long with at least 1 special character, 1 capital letter, 1 small and 1 digit',
+      })
+
+    password: string;
+
+
+    @IsNotEmpty()
+    @Matches(/^(01)[3,5,6,7,8,9][0-9]{8}$/, {
+        message:'Mobile no. is not correct',
+      })
+
+    mbl_no: string;
+
+
 
 
 
