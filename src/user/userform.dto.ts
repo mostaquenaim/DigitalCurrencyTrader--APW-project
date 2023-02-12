112770a676f6ea9a0015f54676771b58cea9a9a3
Ahnaf Ahmed <ahnaf.ahmed.42173@gmail.com>
2023-02-13 00:14:59 +0600

    aa

--- src/user/userform.dto.ts
+++ src/user/userform.dto.ts
@@ -2,19 +2,34 @@
 //     @IsNotEmpty({messege: "enter yout id"})
 //     id:number;
 // }
-import { IsInt, IsNotEmpty, Length } from "class-validator";
+import { MESSAGES } from "@nestjs/core/constants";
+import { IsInt, IsNotEmpty, Length, Matches } from "class-validator";
 export class UserForm {   
-    @IsNotEmpty({message: "Please enter your id"}) 
+    @IsNotEmpty({message: "Please enter your id and it must be a number"}) 
     @IsInt()
+    // @Matches()
     id: number;
 
     @IsNotEmpty()
-    @Length(3,8)
+    // @Length(3,8)
+    @Matches('^[a-z]^+$')
     name: string;
+
+    @IsNotEmpty()
+    // alphanumeric characters and - are valid
+    // you can change this as you like
+    @Matches(/^[a-zA-Z]+$/, {message:'username must not have anything other than letters and numbers'})
+    @Length(4,20)
+    username: string;
 
     // @IsNotEmpty()
     @Length(3)
     currency: string;
+
+    @IsNotEmpty()
+    @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
+        message:'Password must be equal or more than 8 characters long with at least 1 special character, 1 capital letter, 1 small and 1 digit'})
+        password: string;
 
     
 }
