41471ffaa0b2f3eca4f26de71ed270e0f0d9294f
Ahnaf Ahmed <ahnaf.ahmed.42173@gmail.com>
2023-02-12 23:57:31 +0600

    added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Controller, Get } from '@nestjs/common';
-=======
-import { Controller, Get, Query } from '@nestjs/common';
->>>>>>> 568d600f6dbe0da1872fac5af3b6bf964b668da9
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,9 +9,30 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-<<<<<<< HEAD
-=======
+}
+
+
+// <<<<<<< HEAD
+// import { Controller, Get } from '@nestjs/common';
+// =======
+// import { Controller, Get, Query } from '@nestjs/common';
+// >>>>>>> 568d600f6dbe0da1872fac5af3b6bf964b668da9
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// <<<<<<< HEAD
+// =======
  
 
->>>>>>> 568d600f6dbe0da1872fac5af3b6bf964b668da9
-}
+// >>>>>>> 568d600f6dbe0da1872fac5af3b6bf964b668da9
+// }
+
+
+
